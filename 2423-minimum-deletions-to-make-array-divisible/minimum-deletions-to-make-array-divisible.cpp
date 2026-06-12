aae6bc0a651bd1d437a96fb69a3155a4484e9c9b
piyushgohil6302 <piyushgohil6302@gmail.com>
2026-06-12 08:15:51 +0530

    Time: 11 ms (84.14%) | Memory: 92 MB (55.95%) - LeetSync

--- 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.cpp
+++ 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
         int g=numsDivide[0];
-        for(int i=1;i<numsDivide.size();i++){
+        int a=numsDivide.size();
+        int b=nums.size();
+        for(int i=1;i<a;i++){
             g=__gcd(g,numsDivide[i]);
         }
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<b;i++){
             if(g%nums[i]==0){
                 return i;
                 break;
