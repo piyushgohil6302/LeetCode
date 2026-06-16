303df9f3a34ca159ac43ac2c2aea67ce60331d54
piyushgohil6302 <piyushgohil6302@gmail.com>
2026-06-16 08:01:42 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (42.05%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     string processStr(string s) {
         string a = "";
-        for(int i = 0; i < s.size(); i++){
-            if(islower(s[i])){
+        for (int i = 0; i < s.size(); i++) {
+            if (islower(s[i])) {
                 a.push_back(s[i]);
-            }
-            else if(s[i] == '*' && !a.empty()){  // guard pop_back
+            } else if (s[i] == '*' && !a.empty()) {
                 a.pop_back();
-            }
-            else if(s[i] == '#'){                 // safe without guard
+            } else if (s[i] == '#') {
                 a += a;
-            }
-            else if(s[i] == '%'){
+            } else if (s[i] == '%') {
                 reverse(a.begin(), a.end());
             }
         }
