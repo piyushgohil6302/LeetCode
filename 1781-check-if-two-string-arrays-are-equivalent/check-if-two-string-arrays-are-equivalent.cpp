bd63aa7ee24e4aa43083052ef174d8033dff26ad
piyushgohil6302 <piyushgohil6302@gmail.com>
2026-08-12 22:50:25 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (78.98%) - LeetSync

---
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1=word1[0];
+        string s2=word2[0];
+        for(int i=1;i<word1.size();i++){
+            s1+=word1[i];
+        }
+          for(int i=1;i<word2.size();i++){
+            s2+=word2[i];
+        }
+        if(s1==s2){
+            return true;
+        }
+        else{
+            return false;
+        }
+        return 0;
+    }
+};
